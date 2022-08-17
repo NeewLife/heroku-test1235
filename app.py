1e4158ac2a1db8f184713cbd2faf75ad99be2fac
NeewLife <dhtpdud2009@gmail.com>
2022-08-17 12:06:31 +0900

    0817

--- app.py
+++ app.py
@@ -34,5 +34,28 @@ def Message():
     }
     return jsonify(dataSend)
 
+@app.route('/private')
+def private():
+    dataSend = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": "알고 싶으신 민간분양 유형을 눌러주세요."
+                    },
+                    "buttons": [
+                        {
+                            "action": "message",
+                            "label": "일반",
+                            "messageText": "일반공급입니다."
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+    return jsonify(dataSend)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0') # Flask 기본포트 5000번
