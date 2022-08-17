f4aef0b0606c0719748344c325e7d0105d204ea3
NeewLife <dhtpdud2009@gmail.com>
2022-08-17 11:02:05 +0900

    0817

--- app.py
+++ app.py
@@ -1,39 +1,43 @@
-# -*- coding: utf-8 -*-
-from flask import Flask
-import pandas as pd 
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
-
-## DB 연결 Local
-def db_create():
-    # 로컬
-    engine = create_engine("postgresql://postgres:1234@localhost:5432/chatbot", echo = False)
-    
-    # Heroku
-    engine = create_engine("postgresql://xulxpjyjsmiezj:79049b8e66c1f943285e01a2b88b7bf63039616b863bd05e6e5f417d224f5095@ec2-44-206-197-71.compute-1.amazonaws.com:5432/da7bkvmriok0f1", echo = False)
-
-    engine.connect()
-    engine.execute("""
-        CREATE TABLE iris(
-            sepal_length FLOAT NOT NULL,
-            sepal_width FLOAT NOT NULL,
-            pepal_length FLOAT NOT NULL,
-            pepal_width FLOAT NOT NULL,
-            species VARCHAR(100) NOT NULL
-        );"""
-    )
-    data = pd.read_csv('data/iris.csv')
-    print(data)
-    data.to_sql(name='iris', con=engine, schema = 'public', if_exists='replace', index=False)
-
+from flask import Flask, request, jsonify
+import sys
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    db_create()
-    return "Hello World!"
 
+@app.route('/keyboard')
+def Keyboard():
+    dataSend = {
+    "Subject":"OSSP",
+    "user":"corona_chatbot"
+    }
+    return jsonify(dataSend)
 
+@app.route('/message', methods=['POST'])
+def Message():
+
+    content = request.get_json()
+    content = content['userRequest']
+    content = content['utterance']
+
+    if content == u"안녕":
+        dataSend = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "carousel": {
+                            "type" : "basicCard",
+                            "items": [
+                                {
+                                    "title" : "",
+                                    "description" : "안녕하세요"
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+    return jsonify(dataSend)
 
 if __name__ == "__main__":
-    db_create()
-    app.run()
+    app.run(host='0.0.0.0') # Flask 기본포트 5000번
