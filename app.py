ece474bd46df77e977d1f3cabd7351daf0575222
NeewLife <dhtpdud2009@gmail.com>
2022-08-18 09:41:25 +0900

    initial updated

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
 from flask import Flask, request, jsonify
+import pandas as pd
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 import sys
+
 app = Flask(__name__)
+
+## DB 연결 Local
+def db_create():
+    # 로컬
+    engine = create_engine("postgresql://postgres:1234@localhost:5432/chatbot", echo = False)
+    
+    # Heroku
+    engine = create_engine("postgresql://avoldyshprcjzu:2a92b7788746e69afe72137c1021fdb9966e4ceb2c41ad68c71e7aedd7e65db3@ec2-107-23-76-12.compute-1.amazonaws.com:5432/d7njsi7ltrih2l", echo = False)
+
+    engine.connect()
+    engine.execute("""
+        CREATE TABLE IF NOT EXISTS iris(
+            sepal_length FLOAT NOT NULL,
+            sepal_width FLOAT NOT NULL,
+            pepal_length FLOAT NOT NULL,
+            pepal_width FLOAT NOT NULL,
+            species VARCHAR(100) NOT NULL
+        );"""
+    )
+    data = pd.read_csv('data/iris.csv')
+    print(data)
+    data.to_sql(name='iris', con=engine, schema = 'public', if_exists='replace', index=False)
+
 
 @app.route("/")
 def hello():
@@ -254,4 +280,5 @@ def private3():
     return jsonify(dataSend)
 
 if __name__ == "__main__":
+    db_create()
     app.run(host='0.0.0.0') # Flask 기본포트 5000번
